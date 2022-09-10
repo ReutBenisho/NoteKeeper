ca05905b985c9b8cd593c438997820c00c61f44f
Reut Benisho <xreutx197@gmail.com>
2022-09-10 23:19:34 +0300

    Added note list activity + populated the list with data

---
+++ app/src/main/java/com/example/notekeeper/NoteListActivity.java
@@ -0,0 +1,54 @@
+package com.example.notekeeper;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.notekeeper.databinding.ActivityNoteListBinding;
+
+import java.util.List;
+
+public class NoteListActivity extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityNoteListBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityNoteListBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.toolbar);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        initializeDisplayContent();
+    }
+
+    private void initializeDisplayContent() {
+        ListView listNotes = findViewById(R.id.list_notes);
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        ArrayAdapter<NoteInfo> adapterNotes = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, notes);
+        listNotes.setAdapter(adapterNotes);
+    }
+
+}
