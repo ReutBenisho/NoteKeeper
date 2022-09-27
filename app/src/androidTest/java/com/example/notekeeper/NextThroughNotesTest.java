cc0d1dffbf648616ff8090e671d8e265eeaf230f
Reut Benisho <xreutx197@gmail.com>
2022-09-28 00:40:13 +0300

    Added unit test for the navigation drawer

---
+++ app/src/androidTest/java/com/example/notekeeper/NextThroughNotesTest.java
@@ -0,0 +1,55 @@
+package com.example.notekeeper;
+
+//import static android.support.test.espresso.contrib.DrawerMatchers.isClosed;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.DrawerMatchers.isClosed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withSpinnerText;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+//import android.support.test.espresso.contrib.DrawerActions;
+//import android.support.test.espresso.contrib.NavigationViewActions;
+//import android.support.test.espresso.contrib.RecyclerViewActions;
+//import android.view.Gravity;
+
+import android.view.Gravity;
+
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.NavigationViewActions;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.List;
+
+
+public class NextThroughNotesTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule =
+            new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void NextThroughNotes(){
+
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
+                .perform(DrawerActions.open()); // Open Drawer
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_notes));
+        onView(withId(R.id.list_items)).perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
+
+        List<NoteInfo> notes = DataManager.getInstance().getNotes();
+        int index = 0;
+        NoteInfo note = notes.get(index);
+        onView(withId(R.id.spinner_courses)).check(
+                matches(withSpinnerText(note.getCourse().getTitle()))
+        );
+        onView(withId(R.id.text_note_title)).check(matches(withText(note.getTitle())));
+        onView(withId(R.id.text_note_text)).check(matches(withText(note.getText())));
+
+    }
+
+}
